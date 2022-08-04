10592ac79b3d627c0fcf319f0c41783aeb448124
Hennadii Chernyshchyk <genaloner@gmail.com>
2022-08-04 20:44:13 +0300

    Use unwrap_or_else

--- src/core/errors.rs
+++ src/core/errors.rs
@@ -3,7 +3,5 @@ use bevy::prelude::*;
 
 /// A system to enable early return with error logging as errors.
 pub(super) fn log_err_system(In(result): In<Result<()>>) {
-    if let Err(error) = result {
-        error!("{error:#}")
-    }
+    result.unwrap_or_else(|e| error!("{e:#}"));
 }
