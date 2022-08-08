c3e056876a94d7453122d23a7ddc4df4d2e5dd56
Hennadii Chernyshchyk <genaloner@gmail.com>
2022-08-08 13:33:52 +0300

    Show errors output in tests

--- src/core/errors.rs
+++ src/core/errors.rs
@@ -3,5 +3,8 @@ use bevy::prelude::*;
 
 /// A system to enable early return with error logging as errors.
 pub(super) fn log_err_system(In(result): In<Result<()>>) {
+    #[cfg(not(test))]
     result.unwrap_or_else(|e| error!("{e:#}"));
+    #[cfg(test)]
+    result.unwrap_or_else(|e| eprintln!("{e:#}"));
 }
