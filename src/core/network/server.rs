4f35c521c6e44f27926b7fd1c76ebce031e50f86
Hennadii Chernyshchyk <genaloner@gmail.com>
2022-09-15 20:42:40 +0300

    Remove IP from server settings

--- src/core/network/server.rs
+++ src/core/network/server.rs
@@ -2,7 +2,7 @@ use anyhow::Result;
 use bevy::prelude::*;
 use bevy_renet::renet::{RenetConnectionConfig, RenetServer, ServerAuthentication, ServerConfig};
 use clap::Args;
-use std::net::{SocketAddr, UdpSocket};
+use std::net::{Ipv4Addr, SocketAddr, UdpSocket};
 use std::time::SystemTime;
 
 use super::{Channel, DEFAULT_PORT, MAX_CLIENTS, PROTOCOL_ID};
@@ -21,10 +21,6 @@ pub(crate) struct ServerSettings {
     #[clap(short, long, default_value_t = ServerSettings::default().server_name)]
     pub(crate) server_name: String,
 
-    /// IP address to bind.
-    #[clap(short, long, default_value_t = ServerSettings::default().ip)]
-    pub(crate) ip: String,
-
     /// Port to use.
     #[clap(short, long, default_value_t = ServerSettings::default().port)]
     pub(crate) port: u16,
@@ -34,7 +30,6 @@ impl Default for ServerSettings {
     fn default() -> Self {
         Self {
             server_name: "My game".to_string(),
-            ip: "127.0.0.1".to_string(),
             port: DEFAULT_PORT,
         }
     }
@@ -43,7 +38,7 @@ impl Default for ServerSettings {
 impl ServerSettings {
     pub(crate) fn create_server(&self) -> Result<RenetServer> {
         let current_time = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH)?;
-        let server_addr = SocketAddr::new(self.ip.parse()?, self.port);
+        let server_addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), self.port);
         let socket = UdpSocket::bind(server_addr)?;
         let server_config = ServerConfig::new(
             MAX_CLIENTS,
