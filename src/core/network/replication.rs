41c047c8575b8f8b4c374ee948a5e10d06bc1a89
Hennadii Chernyshchyk <genaloner@gmail.com>
2022-09-18 21:58:07 +0300

    Refactor replication logic

--- src/core/network/replication.rs
+++ src/core/network/replication.rs
@@ -60,13 +60,13 @@ impl Plugin for ReplicationPlugin {
                 ReplicationStage::Tick,
                 FixedTimestepStage::new(Duration::from_secs_f64(Self::TIMESTEP)).with_stage(
                     SystemStage::single(
-                        Self::send_server_message_system.run_if_resource_exists::<RenetServer>(),
+                        Self::world_diffs_sending_system.run_if_resource_exists::<RenetServer>(),
                     ),
                 ),
             )
-            .add_system(Self::client_acks_cleanup_system.run_if_resource_exists::<RenetServer>())
-            .add_system(Self::receive_client_ack_system.run_if_resource_exists::<RenetServer>())
-            .add_system(Self::send_client_ack_system.run_if_resource_exists::<RenetClient>())
+            .add_system(Self::tick_ack_sending_system.run_if_resource_exists::<RenetClient>())
+            .add_system(Self::tick_acks_receiving_system.run_if_resource_exists::<RenetServer>())
+            .add_system(Self::tick_acks_cleanup_system.run_if_resource_exists::<RenetServer>())
             .add_system(Self::server_reset_system.run_if_resource_removed::<RenetServer>())
             .add_system(Self::client_reset_system.run_if_resource_removed::<RenetClient>());
 
@@ -74,7 +74,7 @@ impl Plugin for ReplicationPlugin {
             // To avoid ambiguity: https://github.com/IyesGames/iyes_loopless/issues/15
             use iyes_loopless::condition::IntoConditionalExclusiveSystem;
             app.add_system(
-                Self::receive_server_message_system
+                Self::world_diff_receiving_system
                     .run_if_resource_exists::<RenetClient>()
                     .at_start(),
             );
@@ -85,42 +85,7 @@ impl Plugin for ReplicationPlugin {
 impl ReplicationPlugin {
     const TIMESTEP: f64 = 0.1;
 
-    fn client_acks_cleanup_system(
-        mut server_events: EventReader<ServerEvent>,
-        mut client_acks: ResMut<ClientAcks>,
-    ) {
-        for event in server_events.iter() {
-            if let ServerEvent::ClientDisconnected(id) = event {
-                client_acks.remove(id);
-            }
-        }
-    }
-
-    fn receive_client_ack_system(
-        mut client_acks: ResMut<ClientAcks>,
-        mut server: ResMut<RenetServer>,
-    ) {
-        for client_id in server.clients_id() {
-            let mut received_acks = Vec::<TickAck>::new();
-            while let Some(message) = server.receive_message(client_id, Channel::Replication as u8)
-            {
-                if let Ok(tick_ack) = rmp_serde::from_slice(&message)
-                    .tap_err(|e| error!("unable to deserialize message from client: {e:#?}"))
-                {
-                    received_acks.push(tick_ack);
-                }
-            }
-
-            if let Some(tick_ack) = received_acks.iter().max_by_key(|tick_ack| tick_ack.0) {
-                let last_ack = client_acks.entry(client_id).or_default();
-                if *last_ack < tick_ack.0 {
-                    *last_ack = tick_ack.0;
-                }
-            }
-        }
-    }
-
-    fn send_server_message_system(
+    fn world_diffs_sending_system(
         mut set: ParamSet<(&World, ResMut<RenetServer>)>,
         client_acks: Res<ClientAcks>,
         registry: Res<TypeRegistry>,
@@ -147,44 +112,56 @@ impl ReplicationPlugin {
         }
     }
 
-    fn receive_server_message_system(world: &mut World) {
+    fn world_diff_receiving_system(world: &mut World) {
         world.resource_scope(|world, registry: Mut<TypeRegistry>| {
-            let mut received_diffs = Vec::<WorldDiff>::new();
-            let mut client = world.resource_mut::<RenetClient>();
-            while let Some(message) = client.receive_message(Channel::Replication as u8) {
-                let mut deserializer = Deserializer::from_read_ref(&message);
-                if let Ok(world_diff) = WorldDiffDeserializer::new(&registry)
-                    .deserialize(&mut deserializer)
-                    .tap_err(|e| error!("unable to deserialize server world diff: {e}"))
-                {
-                    received_diffs.push(world_diff);
-                }
-            }
-
-            let mut tick_ack = world.resource_mut::<TickAck>();
-            let world_diff = match received_diffs
-                .into_iter()
-                .max_by_key(|world_diff| world_diff.tick)
+            if let Some(world_diff) =
+                receive_world_diff(&mut world.resource_mut::<RenetClient>(), &registry)
             {
-                Some(world_diff) if tick_ack.0 < world_diff.tick => world_diff,
-                _ => return,
-            };
-            tick_ack.0 = world_diff.tick;
+                let mut tick_ack = world.resource_mut::<TickAck>();
+                if tick_ack.0 < world_diff.tick {
+                    tick_ack.0 = world_diff.tick;
+                }
 
-            world.resource_scope(|world, mut entity_map: Mut<NetworkEntityMap>| {
-                apply_diffs(world, &mut entity_map, &registry, world_diff);
-            });
-            if !world.contains_resource::<GameWorld>() {
-                world.insert_resource(GameWorld::default()); // TODO: Replicate this resource.
-                world.insert_resource(NextState(GameState::World));
+                world.resource_scope(|world, mut entity_map: Mut<NetworkEntityMap>| {
+                    apply_diffs(world, &mut entity_map, &registry, world_diff);
+                });
+                if !world.contains_resource::<GameWorld>() {
+                    world.insert_resource(GameWorld::default()); // TODO: Replicate this resource.
+                    world.insert_resource(NextState(GameState::World));
+                }
             }
         });
     }
 
-    fn send_client_ack_system(tick_ack: Res<TickAck>, mut client: ResMut<RenetClient>) {
+    fn tick_ack_sending_system(tick_ack: Res<TickAck>, mut client: ResMut<RenetClient>) {
         let message = rmp_serde::to_vec(&*tick_ack)
             .unwrap_or_else(|e| panic!("client ack should be serialized: {e}"));
         client.send_message(Channel::Replication as u8, message);
+    }
+
+    fn tick_acks_receiving_system(
+        mut client_acks: ResMut<ClientAcks>,
+        mut server: ResMut<RenetServer>,
+    ) {
+        for client_id in server.clients_id() {
+            if let Some(tick_ack) = receive_tick_ack(&mut server, client_id) {
+                let last_ack = client_acks.entry(client_id).or_default();
+                if *last_ack < tick_ack.0 {
+                    *last_ack = tick_ack.0;
+                }
+            }
+        }
+    }
+
+    fn tick_acks_cleanup_system(
+        mut server_events: EventReader<ServerEvent>,
+        mut client_acks: ResMut<ClientAcks>,
+    ) {
+        for event in server_events.iter() {
+            if let ServerEvent::ClientDisconnected(id) = event {
+                client_acks.remove(id);
+            }
+        }
     }
 
     fn server_reset_system(mut commands: Commands) {
@@ -195,6 +172,37 @@ impl ReplicationPlugin {
         commands.insert_resource(TickAck::default());
         commands.insert_resource(NetworkEntityMap::default());
     }
+}
+
+/// Reads all world diff from socket and returns the latest if it was received.
+fn receive_world_diff(client: &mut RenetClient, registry: &TypeRegistry) -> Option<WorldDiff> {
+    let mut received_diffs = Vec::<WorldDiff>::new();
+    while let Some(message) = client.receive_message(Channel::Replication as u8) {
+        let mut deserializer = Deserializer::from_read_ref(&message);
+        if let Ok(world_diff) = WorldDiffDeserializer::new(registry)
+            .deserialize(&mut deserializer)
+            .tap_err(|e| error!("unable to deserialize server world diff: {e}"))
+        {
+            received_diffs.push(world_diff);
+        }
+    }
+
+    received_diffs
+        .into_iter()
+        .max_by_key(|world_diff| world_diff.tick)
+}
+
+fn receive_tick_ack(server: &mut RenetServer, client_id: u64) -> Option<TickAck> {
+    let mut received_acks = Vec::<TickAck>::new();
+    while let Some(message) = server.receive_message(client_id, Channel::Replication as u8) {
+        if let Ok(tick_ack) = rmp_serde::from_slice(&message)
+            .tap_err(|e| error!("unable to deserialize message from client: {e:#?}"))
+        {
+            received_acks.push(tick_ack);
+        }
+    }
+
+    received_acks.into_iter().max_by_key(|tick_ack| tick_ack.0)
 }
 
 fn collect_changes(
@@ -390,7 +398,7 @@ fn apply_component_diff(
             reflect_component.apply_or_insert(world, local_entity, &**reflect);
             if let Some(reflect_map_entities) = registration.data::<ReflectMapEntities>() {
                 reflect_map_entities
-                    .map_entities(world, &entity_map)
+                    .map_entities(world, entity_map)
                     .with_context(|| format!("unable to map entities for {type_name}"))?;
             }
         }
@@ -429,7 +437,7 @@ mod tests {
     };
 
     #[test]
-    fn client_acks_cleanup() {
+    fn tick_acks_cleanup() {
         let mut app = App::new();
         app.add_plugin(TestReplicationMessagingPlugin::new(
             NetworkPreset::ServerAndClient { connected: true },
@@ -449,7 +457,7 @@ mod tests {
     }
 
     #[test]
-    fn ack_receive() {
+    fn tick_acks_receiving() {
         let mut app = App::new();
         app.add_plugin(TestReplicationMessagingPlugin::new(
             NetworkPreset::ServerAndClient { connected: true },
@@ -638,7 +646,7 @@ mod tests {
     }
 
     #[test]
-    fn client_resets() {
+    fn client_reset() {
         let mut app = App::new();
         app.add_plugin(TestReplicationMessagingPlugin::new(NetworkPreset::Client));
 
