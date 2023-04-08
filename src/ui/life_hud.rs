b48cce76c33653a8fdbe6a7deeaeda712cd5d021
Hennadii Chernyshchyk <genaloner@gmail.com>
2023-04-08 20:52:25 +0300

    Add UI for family members

--- src/ui/life_hud.rs
+++ src/ui/life_hud.rs
@@ -6,7 +6,7 @@ use bevy_egui::{
 
 use crate::core::{
     actor::ActiveActor,
-    family::{ActiveFamily, Budget, FamilyMode},
+    family::{ActiveFamily, Actors, Budget, FamilyMode},
     game_state::GameState,
     task::{Task, TaskCancel, TaskQueue, TaskRequestKind, TaskRequestRemove},
 };
@@ -16,7 +16,11 @@ pub(super) struct LifeHudPlugin;
 impl Plugin for LifeHudPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(
-            (Self::active_tasks_system, Self::portrait_system)
+            (
+                Self::active_tasks_system,
+                Self::portrait_system,
+                Self::family_members_system,
+            )
                 .in_set(OnUpdate(GameState::Family))
                 .in_set(OnUpdate(FamilyMode::Life)),
         );
@@ -24,6 +28,7 @@ impl Plugin for LifeHudPlugin {
 }
 
 const ICON_SIZE: f32 = 50.0;
+const PORTRAIT_WIDTH: f32 = 150.0;
 
 impl LifeHudPlugin {
     fn active_tasks_system(
@@ -97,8 +102,49 @@ impl LifeHudPlugin {
                     let budget = active_families.single();
                     ui.label(budget.to_string() + " 💳");
 
-                    const PORTRAIT_WIDTH: f32 = 150.0;
                     ui.allocate_space((PORTRAIT_WIDTH, 0.0).into());
+                });
+            });
+    }
+
+    fn family_members_system(
+        mut commands: Commands,
+        mut egui: EguiContexts,
+        active_families: Query<&Actors, With<ActiveFamily>>,
+        active_actors: Query<Entity, With<ActiveActor>>,
+    ) {
+        let ctx = egui.ctx_mut();
+        let button_padding = ctx.style().spacing.button_padding;
+        let item_spacing = ctx.style().spacing.item_spacing;
+        let left_offset = ICON_SIZE + PORTRAIT_WIDTH + button_padding.x + item_spacing.x;
+
+        Area::new("Family members")
+            .anchor(Align2::LEFT_BOTTOM, (left_offset, 0.0))
+            .show(ctx, |ui| {
+                Frame::canvas(ui.style()).show(ui, |ui| {
+                    ui.horizontal(|ui| {
+                        let mut members_count = 0;
+                        for &entity in active_families.single().iter() {
+                            let button =
+                                ImageButton::new(TextureId::Managed(0), (ICON_SIZE, ICON_SIZE))
+                                    .selected(active_actors.get(entity).is_ok());
+                            if ui.add(button).clicked() {
+                                commands.entity(entity).insert(ActiveActor);
+                                commands
+                                    .entity(active_actors.single())
+                                    .remove::<ActiveActor>();
+                            }
+                            members_count += 1;
+                        }
+
+                        ui.set_visible(false);
+                        const MAX_FAMILY_SIZE: usize = 4;
+                        for _ in members_count..MAX_FAMILY_SIZE {
+                            let button =
+                                ImageButton::new(TextureId::Managed(0), (ICON_SIZE, ICON_SIZE));
+                            ui.add(button);
+                        }
+                    })
                 });
             });
     }
