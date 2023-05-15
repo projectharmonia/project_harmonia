4e1d1c6b82e31f319a9e71a698e4a518b34058ab
Hennadii Chernyshchyk <genaloner@gmail.com>
2023-05-15 18:26:19 +0300

    Add animation events

--- src/core/animation.rs
+++ src/core/animation.rs
@@ -9,7 +9,10 @@ pub(super) struct AnimationPlugin;
 
 impl Plugin for AnimationPlugin {
     fn build(&self, app: &mut App) {
-        app.add_system(Self::playing_system.in_set(OnUpdate(WorldState::InWorld)));
+        app.add_event::<AnimationEnded>().add_systems(
+            (Self::playing_system, Self::init_system, Self::update_system)
+                .in_set(OnUpdate(WorldState::InWorld)),
+        );
     }
 }
 
@@ -18,22 +21,64 @@ impl AnimationPlugin {
     ///
     /// Makes the behavior similar to adding [`Handle<Scene>`].
     fn playing_system(
-        actors: Query<
+        mut commands: Commands,
+        scenes: Query<
             (Entity, &Handle<AnimationClip>),
             Or<(Changed<Handle<AnimationClip>>, Added<SceneHooked>)>,
         >,
         children: Query<&Children>,
         mut animation_players: Query<&mut AnimationPlayer>,
     ) {
-        for (entity, handle) in &actors {
+        for (entity, animation_handle) in &scenes {
             if let Some(mut animation_player) = animation_players
                 .iter_many_mut(children.iter_descendants(entity))
                 .fetch_next()
             {
+                commands.entity(entity).remove::<AnimationTimer>();
                 animation_player
-                    .play_with_transition(handle.clone(), Duration::from_millis(200))
+                    .play_with_transition(animation_handle.clone(), Duration::from_millis(200))
                     .repeat();
             }
         }
     }
+
+    /// Inserts [`AnimationTimer`] after animation loading.
+    fn init_system(
+        mut commands: Commands,
+        animations: Res<Assets<AnimationClip>>,
+        scenes: Query<(Entity, &Handle<AnimationClip>), Without<AnimationTimer>>,
+    ) {
+        for (entity, animation_handle) in &scenes {
+            if let Some(animation) = animations.get(animation_handle) {
+                commands
+                    .entity(entity)
+                    .insert(AnimationTimer::new(animation.duration()));
+            }
+        }
+    }
+
+    fn update_system(
+        time: Res<Time>,
+        mut end_events: EventWriter<AnimationEnded>,
+        mut scenes: Query<(Entity, &mut AnimationTimer)>,
+    ) {
+        for (entity, mut timer) in &mut scenes {
+            timer.tick(time.delta());
+            if timer.just_finished() {
+                end_events.send(AnimationEnded(entity));
+            }
+        }
+    }
 }
+
+/// Tracks animation elapsed time to notify when it finishes.
+#[derive(Component, Deref, DerefMut)]
+struct AnimationTimer(Timer);
+
+impl AnimationTimer {
+    fn new(duration: f32) -> Self {
+        Self(Timer::from_seconds(duration, TimerMode::Repeating))
+    }
+}
+
+pub(super) struct AnimationEnded(pub(super) Entity);
