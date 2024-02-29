1386124c886963ef36e4a03c31f4f8cbe9ac3cfa
Hennadii Chernyshchyk <genaloner@gmail.com>
2024-02-29 23:13:36 +0200

    Dynamically spawn and despawn 2D camera

--- src/ui/camera_2d.rs
+++ src/ui/camera_2d.rs
@@ -1,21 +1,25 @@
 use bevy::prelude::*;
 
+use crate::core::game_state::GameState;
+
 pub(super) struct Camera2dPlugin;
 
 impl Plugin for Camera2dPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, Self::setup_system);
+        app.add_systems(Startup, Self::spawn_system)
+            .add_systems(OnExit(GameState::City), Self::spawn_system)
+            .add_systems(OnExit(GameState::Family), Self::spawn_system)
+            .add_systems(OnEnter(GameState::City), Self::despawn_system)
+            .add_systems(OnEnter(GameState::Family), Self::despawn_system);
     }
 }
 
 impl Camera2dPlugin {
-    fn setup_system(mut commands: Commands) {
-        commands.spawn(Camera2dBundle {
-            camera: Camera {
-                order: -1,
-                ..Default::default()
-            },
-            ..Default::default()
-        });
+    fn spawn_system(mut commands: Commands) {
+        commands.spawn(Camera2dBundle::default());
+    }
+
+    fn despawn_system(mut commands: Commands, cameras: Query<Entity, With<Camera2d>>) {
+        commands.entity(cameras.single()).despawn();
     }
 }
