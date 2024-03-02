bd37e47bbc0b62379a5217e82c66b01b5560285a
Hennadii Chernyshchyk <genaloner@gmail.com>
2024-03-02 02:03:55 +0200

    Do not use 2D camera in family editor

--- src/ui/camera_2d.rs
+++ src/ui/camera_2d.rs
@@ -7,10 +7,12 @@ pub(super) struct Camera2dPlugin;
 impl Plugin for Camera2dPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, Self::spawn_system)
-            .add_systems(OnExit(GameState::City), Self::spawn_system)
+            .add_systems(OnExit(GameState::FamilyEditor), Self::spawn_system)
             .add_systems(OnExit(GameState::Family), Self::spawn_system)
-            .add_systems(OnEnter(GameState::City), Self::despawn_system)
-            .add_systems(OnEnter(GameState::Family), Self::despawn_system);
+            .add_systems(OnExit(GameState::City), Self::spawn_system)
+            .add_systems(OnEnter(GameState::FamilyEditor), Self::despawn_system)
+            .add_systems(OnEnter(GameState::Family), Self::despawn_system)
+            .add_systems(OnEnter(GameState::City), Self::despawn_system);
     }
 }
 
