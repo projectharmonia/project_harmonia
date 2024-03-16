c98b66e70b45954df3851a25128c90076f46954c
Hennadii Chernyshchyk <genaloner@gmail.com>
2024-03-16 23:15:03 +0200

    Register `Polygon`

--- src/core/math.rs
+++ src/core/math.rs
@@ -3,12 +3,13 @@ pub(super) mod segment;
 
 use bevy::prelude::*;
 
+use polygon::Polygon;
 use segment::Segment;
 
 pub(super) struct MathPlugin;
 
 impl Plugin for MathPlugin {
     fn build(&self, app: &mut App) {
-        app.register_type::<Segment>();
+        app.register_type::<Polygon>().register_type::<Segment>();
     }
 }
