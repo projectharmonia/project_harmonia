a2834be07ec5ebadecb16fd2ce0ec8b94491efcb
Hennadii Chernyshchyk <genaloner@gmail.com>
2024-04-06 23:20:50 +0300

    Bump oxidized_navigation to a038eb

--- src/main.rs
+++ src/main.rs
@@ -60,22 +60,15 @@ fn main() {
             WireframePlugin,
             AtmospherePlugin,
             InputManagerPlugin::<Action>::default(),
-            OxidizedNavigationPlugin::<Collider>::new(NavMeshSettings {
-                cell_width: ACTOR_RADIUS / 2.0,
-                cell_height: ACTOR_RADIUS / 4.0,
-                tile_width: 100,
-                world_half_extents: HALF_CITY_SIZE,
-                world_bottom_bound: 0.0,
-                max_traversable_slope_radians: 40_f32.to_radians(),
-                walkable_height: (ACTOR_HEIGHT * ACTOR_RADIUS / 4.0).ceil() as u16,
-                walkable_radius: 1,
-                step_height: 3,
-                min_region_area: 100,
-                merge_region_area: 500,
-                max_contour_simplification_error: 1.1,
-                max_edge_length: 80,
-                max_tile_generation_tasks: None,
-            }),
+            OxidizedNavigationPlugin::<Collider>::new(
+                NavMeshSettings::from_agent_and_bounds(
+                    ACTOR_RADIUS,
+                    ACTOR_HEIGHT,
+                    HALF_CITY_SIZE,
+                    0.0,
+                )
+                .with_walkable_radius(1),
+            ),
             OxidizedNavigationDebugDrawPlugin,
             PhysicsPlugins::default()
                 .build()
