26b02f37b002ffd9cdbad936ec748fe828bd1413
Hennadii Chernyshchyk <genaloner@gmail.com>
2025-03-02 16:08:58 +0200

    Utilize event bubbling

--- ui/src/error_dialog.rs
+++ ui/src/error_dialog.rs
@@ -21,39 +21,31 @@ fn show(
 ) {
     info!("showing error dialog");
     commands.entity(*root_entity).with_children(|parent| {
-        parent.spawn(Dialog).with_children(|parent| {
-            parent
-                .spawn((
-                    Node {
-                        flex_direction: FlexDirection::Column,
-                        justify_content: JustifyContent::Center,
-                        align_items: AlignItems::Center,
-                        padding: theme.padding.normal,
-                        row_gap: theme.gap.normal,
-                        ..Default::default()
-                    },
-                    theme.panel_background,
-                ))
-                .with_children(|parent| {
-                    parent.spawn((LabelKind::Normal, Text::new(&**trigger)));
-                    parent.spawn(ButtonKind::Normal).with_child(Text::new("Ok"));
-                })
-                .observe(close);
-        });
+        parent
+            .spawn(Dialog)
+            .with_children(|parent| {
+                parent
+                    .spawn((
+                        Node {
+                            flex_direction: FlexDirection::Column,
+                            justify_content: JustifyContent::Center,
+                            align_items: AlignItems::Center,
+                            padding: theme.padding.normal,
+                            row_gap: theme.gap.normal,
+                            ..Default::default()
+                        },
+                        theme.panel_background,
+                    ))
+                    .with_children(|parent| {
+                        parent.spawn((LabelKind::Normal, Text::new(&**trigger)));
+                        parent.spawn(ButtonKind::Normal).with_child(Text::new("Ok"));
+                    });
+            })
+            .observe(close);
     });
 }
 
-fn close(
-    trigger: Trigger<Pointer<Click>>,
-    mut commands: Commands,
-    dialogs: Query<(), With<Dialog>>,
-    parents: Query<&Parent>,
-) {
-    let entity = parents
-        .iter_ancestors(trigger.entity())
-        .find(|entity| dialogs.get(*entity).is_ok())
-        .expect("button should be a part of the error dialog");
-
+fn close(trigger: Trigger<Pointer<Click>>, mut commands: Commands) {
     info!("closing error dialog");
-    commands.entity(entity).despawn_recursive();
+    commands.entity(trigger.entity()).despawn_recursive();
 }
